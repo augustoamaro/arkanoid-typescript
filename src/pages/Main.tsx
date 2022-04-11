cde31d61e00c0115fa297eca18d54f01f2e18988
Augusto <augustoamaro@icloud.com>
2022-04-10 23:25:22 -0300

    Initial commit

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/pages/main.module.scss";
+
+const Main = () => {
+  return (
+    <div className={styles["container"]}>
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default Main;
